c94f5b9eeb81032d074f65e8a1dc4f4cbfcc9cec
133g <guo.no.244@gmail.com>
2025-07-01 12:46:05 +0900

    add im setting for mac

--- nvim/init.lua
+++ nvim/init.lua
@@ -207,5 +207,18 @@ if vim.fn.executable('zenhan') == 1 then
   })
 end
 
+if vim.fn.executable('im-select') == 1 then
+  vim.api.nvim_create_autocmd('InsertLeave', {
+    callback = function()
+      vim.fn.system('im-select com.apple.keylayout.UnicodeHexInput')
+    end
+  })
+  vim.api.nvim_create_autocmd('CmdlineLeave', {
+    callback = function()
+      vim.fn.system('im-select 0')
+    end
+  })
+end
+
 -- 初期化完了通知
 vim.notify('initialization completed')
