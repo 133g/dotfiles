a164d042aa1c64c38c0e24e70ba647172b4465d8
133g <guo.no.244@gmail.com>
2025-07-20 19:58:22 +0900

    update windows terminal setting & bin

--- bin/install_windows_terminal.sh
+++ bin/install_windows_terminal.sh
@@ -2,4 +2,4 @@
 
 windowsUserProfile=/mnt/c/Users/$(cmd.exe /c "echo %USERNAME%" 2>/dev/null | tr -d '\r')
 
-cp ../windows-terminal/settings.json ${windowsUserProfile}/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
+cp ~/dotfiles/windows-terminal/settings.json ${windowsUserProfile}/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
