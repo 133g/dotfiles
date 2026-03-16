446bb8a9b1cabacf74c8e974f652a669e98b06fa
133g <guo.no.244@gmail.com>
2026-03-16 16:10:05 +0900

    wslでwin32yankによるコピペが正しく実行できない問題を修正

--- nvim/lua/config/wsl.lua
+++ nvim/lua/config/wsl.lua
@@ -2,12 +2,25 @@ if vim.fn.has("wsl") == 1 then
 	vim.g.clipboard = {
 		name = "WslClipboard",
 		copy = {
-			["+"] = "win32yank.exe -i --crlf",
-			["*"] = "win32yank.exe -i --crlf",
+			["+"] = { "win32yank.exe", "-i", "--crlf" },
+			["*"] = { "win32yank.exe", "-i", "--crlf" },
 		},
 		paste = {
-			["+"] = "win32yank.exe -o --lf",
-			["*"] = "win32yank.exe -o --lf",
+			["+"] = { "win32yank.exe", "-o", "--lf" },
+			["*"] = { "win32yank.exe", "-o", "--lf" },
+		},
+		cache_enabled = 1,
+	}
+elseif vim.fn.has("win32") == 1 then
+	vim.g.clipboard = {
+		name = "Win32Clipboard",
+		copy = {
+			["+"] = { "C:/bin/win32yank.exe", "-i", "--crlf" },
+			["*"] = { "C:/bin/win32yank.exe", "-i", "--crlf" },
+		},
+		paste = {
+			["+"] = { "C:/bin/win32yank.exe", "-o", "--lf" },
+			["*"] = { "C:/bin/win32yank.exe", "-o", "--lf" },
 		},
 		cache_enabled = 1,
 	}
